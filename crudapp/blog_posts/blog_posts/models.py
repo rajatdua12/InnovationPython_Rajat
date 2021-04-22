2560dea865a26f5715bd55330071bbc6d4b4596f
Rajat Dua <71475115+rajatdua12@users.noreply.github.com>
2021-04-22 12:49:45 -0500

    Delete crudapp/blog_posts directory

--- crudapp/blog_posts/blog_posts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class CrudApp(models.Model):
-    Name = models.CharField(max_length=10)
-    Email = models.EmailField(max_length=30)
-
-    def __str__(self):
-        return self.Name
-
--- crudapp/blog_posts/blog_posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from blog_posts import views
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('new', views.post_create, name='post_new'),
-    path('edit/<int:pk>', views.post_update, name='post_edit'),
-    path('delete/<int:pk>', views.post_delete, name='post_delete'),
-]
